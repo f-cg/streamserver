4ff65ddd5fc17baf2909516aadfd79156679add5
fcg <f.cg@qq.com>
2020-08-11 14:57:58 +0800

    add dameng jdbc driver and connection config

--- src/main/java/com/founder/Constants.java
+++ src/main/java/com/founder/Constants.java
@@ -9,4 +9,8 @@ public class Constants {
 	public static final int JAVALINWEBPORT=7776;
 	public static final String UNISERVERHOST="127.0.0.1";
 	public static final int parallel=1;
+	public static final String dmUrl="jdbc:dm://162.105.146.37:5326";
+	public static final String dmUserName="FOUNDER";
+	public static final String dmPassword="fdOAondameng";
+	public static final String dmJDBC="dm.jdbc.driver.DmDriver";
 }
