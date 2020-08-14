4fa6b2cd642c01f21eb4f1a81a7a019fcea33177
fcg <f.cg@qq.com>
2020-08-14 20:36:05 +0800

    formated

--- src/main/java/com/founder/Constants.java
+++ src/main/java/com/founder/Constants.java
@@ -1,16 +1,16 @@
 package com.founder;
 
 /**
-* Constants
-*/
+ * Constants
+ */
 public class Constants {
 
-	public static final int UNISERVERPORT=8848;
-	public static final int JAVALINWEBPORT=7776;
-	public static final String UNISERVERHOST="127.0.0.1";
-	public static final int parallel=1;
-	public static final String dmUrl="jdbc:dm://162.105.146.37:5326";
-	public static final String dmUserName="FOUNDER";
-	public static final String dmPassword="fdOAondameng";
-	public static final String dmJDBC="dm.jdbc.driver.DmDriver";
+	public static final int UNISERVERPORT = 8848;
+	public static final int JAVALINWEBPORT = 7776;
+	public static final String UNISERVERHOST = "127.0.0.1";
+	public static final int parallel = 1;
+	public static final String dmUrl = "jdbc:dm://162.105.146.37:5326";
+	public static final String dmUserName = "FOUNDER";
+	public static final String dmPassword = "fdOAondameng";
+	public static final String dmJDBC = "dm.jdbc.driver.DmDriver";
 }
